4149e0c3cd1ff0110e7874de93a7d05c177bf667
ajapon88 <ajapon88@gmail.com>
2019-12-16 01:22:29 +0900

    add build script

---
+++ unity-build/Assets/Editor/BuildPlayer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildPlayer
+{
+    private BuildTarget buildTarget;
+
+    private BuildTarget BuildTarget
+    {
+        get
+        {
+            if (buildTarget == default(BuildTarget))
+            {
+                buildTarget = EditorUserBuildSettings.activeBuildTarget;
+            }
+            return buildTarget;
+        }
+        set
+        {
+            buildTarget = value;
+        }
+    }
+
+    [MenuItem("Project/Build/Player")]
+    public static void Build()
+    {
+        new BuildPlayer().Execute();
+    }
+
+    public static void Build_iOS()
+    {
+        new BuildPlayer()
+        {
+            BuildTarget = BuildTarget.iOS,
+        }.Execute();
+    }
+
+    public static void Build_Android()
+    {
+        new BuildPlayer()
+        {
+            BuildTarget = BuildTarget.Android,
+        }.Execute();
+    }
+
+    private void Execute()
+    {
+        string locationPathName = System.IO.Path.Combine(Application.dataPath, "../Build/", BuildTarget.ToString());
+        if (BuildTarget == BuildTarget.Android)
+        {
+            string extension = EditorUserBuildSettings.buildAppBundle ? ".aab" : ".apk";
+            locationPathName = System.IO.Path.Combine(locationPathName, $"{Application.productName}{extension}");
+        }
+        var options = new BuildPlayerOptions
+        {
+            target = BuildTarget,
+            targetGroup = BuildTarget.ToBuildTargetGroup(),
+            locationPathName = locationPathName,
+            scenes = EditorBuildSettings.scenes.Select(x => x.path).ToArray()
+        };
+
+        BuildPipeline.BuildPlayer(options);
+    }
+}
